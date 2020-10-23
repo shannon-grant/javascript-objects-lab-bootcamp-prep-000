51eda9f55411762ca7dfddec964275b14b61f0ec
Shannon Grant <grant.shannon90@gmail.com>
2020-10-23 01:06:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-recipes = {}
+var recipes = {}
 
 function updateObjectWithKeyAndValue(object,key,value) {
   return Object.assign({}, obj, {[key]: value})
-  return obj
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
