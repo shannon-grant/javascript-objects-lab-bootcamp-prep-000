fe01281a4ae7d6f411f1330a7db20e12c9efd6b9
Shannon Grant <grant.shannon90@gmail.com>
2020-10-23 00:59:40 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function updateObjectWithKeyAndValue(object,key,value) {
+
+}
+
+function updateObjectWithKeyAndValue(object,key,value) {
+
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
+
+}
+
+function deleteFromObjectByKey(object, key) {
+
+}
+
+function deleteFromObjectByKey(object, key) {
+
+}
+
+function destructivelyDeleteObjectWithKeyAndValue(object, key) {
+
+}
+
+function destructivelyDeleteObjectWithKeyAndValue(object,key) {
+
+}
